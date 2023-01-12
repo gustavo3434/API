af94009f323f275aa22a7aadb1c9fdfcd9466d58
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-12 19:35:46 -0300

    25 commit

--- js/tiendaAf.js
+++ js/tiendaAf.js
@@ -165,10 +165,11 @@ agregar.forEach(el => {
     });
 })
 const agregarACarrito = (id) =>{ //FUNCION PARA AGREGAR PRODUCTOS AL CARRITO
+    let productoEncontrado = []
     fetch("./data.json")
         .then((res) => res.json())
         .then((data) => {
-            let productoEncontrado = data.find(prod => prod.id === parseInt(id))
+            productoEncontrado = data.find(prod => prod.id === parseInt(id))
             console.log(data)
         }); 
     if(productosCarrito.some((el) => el.id == productoEncontrado.id)){
