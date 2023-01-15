15e7700df6859295553851e4d99ef3bcded47d50
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-14 22:47:58 -0300

    55 commit

--- js/tiendaAf.js
+++ js/tiendaAf.js
@@ -64,15 +64,15 @@ const cargarCarrito = (array,para) => {
         })
     }
     para.innerHTML = tabla
-    /*eliminar = document.querySelectorAll(".eliminar")
+    eliminar = document.querySelectorAll(".eliminar")
     eliminar.forEach(el => {
         el.addEventListener("click", (e) => {
             eliminarDeCarrito(e.target.id)
         });
-    })*/
+    })
 }
 
-/*function eliminarDeCarrito(id){ //FUNCION PARA ELIMINAR PRODUCTOS AL CARRITO
+function eliminarDeCarrito(id){ //FUNCION PARA ELIMINAR PRODUCTOS AL CARRITO
     let productoEncontrado = carritoJS.find(prod => prod.id === parseInt(id))
     carritoJS = carritoJS.filter((item) => item !== productoEncontrado)
     if (carritoJS.length > 0) {
@@ -89,7 +89,7 @@ const cargarCarrito = (array,para) => {
         idCarrito.appendChild(carritoVacio)
         ocu = document.querySelector(".scrols").style.display = "none"
     }
-}*/
+}
 
 /*const filtrado = () => { //FUNCION DE FILTRADO DE PRODUCTOS 
     let parametro = inputSearch.value.trim().toUpperCase()
@@ -118,11 +118,11 @@ const cargarProductos = (array,para) => {
     })
 }
 
-/*agregar.forEach(el => {
+agregar.forEach(el => {
     el.addEventListener("click", (e) => {
         agregarACarrito(e.target.id)
     });
-})*/
+})
 
 let productosCarrito = []
 function agregarACarrito(id){ //FUNCION PARA AGREGAR PRODUCTOS AL CARRITO
