bc0bf594bea0bd5f3347cca95bbd280911a9c6cb
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-17 21:04:45 -0300

    88 commit

---
+++ js/compra.js
@@ -0,0 +1,29 @@
+let confirmar = document.querySelector(".confirmar")
+const nombre = document.querySelector(".nombre")
+const apellido = document.querySelector(".apellido")
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };// FUNCION PARA GUARDAR DATOS EN LOCALSTORAGE 
+
+confirmar.addEventListener("click",(e) => {
+    swal({
+        title: "ariel",
+        text: nombre.value.trim()+ " "+apellido.value.trim()+" desea finalizar la compra?",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((will) => {
+        function redireccionar(){window.location="./index.html";}
+        if (will) {
+          swal("La compra fue finalizada con exito", {
+            icon: "success",
+          })
+          carritoJS = []
+          guardarLocal("listaProductos", JSON.stringify(carritoJS));
+          setTimeout (redireccionar, 1400);
+        } else {
+          swal("La compra fue cancelada");
+          setTimeout (redireccionar, 1400);
+        }
+      });
+      
+})
