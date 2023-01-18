257713f135fec431f6649394c79f935fa6276bef
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-18 11:58:23 -0300

    92 commit

--- js/compra.js
+++ js/compra.js
@@ -5,25 +5,26 @@ const apellido = document.querySelector(".apellido")
 const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };// FUNCION PARA GUARDAR DATOS EN LOCALSTORAGE 
 
 confirmar.addEventListener("click",(e) => {
-    swal({
-        title: "Tieanda AF",
-        text: nombre.value.trim()+ " "+apellido.value.trim()+" desea finalizar la compra?",
-        buttons: true,
-        dangerMode: true,
+  e.preventDefault()
+  swal({
+    title: "Tieanda AF",
+    text: nombre.value.trim()+ " "+apellido.value.trim()+" desea finalizar la compra?",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((will) => {
+    function redireccionar(){window.location="./index.html";}
+    if (will) {
+      swal("La compra fue finalizada con exito, !GRACIAS POR ELEGIR TIENDA AF!", {
+       icon: "success",
       })
-      .then((will) => {
-        function redireccionar(){window.location="./index.html";}
-        if (will) {
-          swal("La compra fue finalizada con exito, !GRACIAS POR ELEGIR TIENDA AF!", {
-            icon: "success",
-          })
-          carritoJS = []
-          guardarLocal("listaProductos", JSON.stringify(carritoJS));
-          setTimeout (redireccionar, 1900);
-        } else {
-          swal("La compra fue cancelada");
-          setTimeout (redireccionar, 1900);
-        }
-      });
+      carritoJS = []
+      guardarLocal("listaProductos", JSON.stringify(carritoJS));
+      setTimeout (redireccionar, 1900);
+      } else {
+      swal("La compra fue cancelada");
+      setTimeout (redireccionar, 1900);
+      }
+  });
       
 })
