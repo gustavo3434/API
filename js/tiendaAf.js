fe73cef0d94e78e8b8baceaa1d23fa7efe3406e2
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-19 17:58:50 -0300

    112 commit

--- js/tiendaAf.js
+++ js/tiendaAf.js
@@ -200,9 +200,31 @@ function totalCarrito (array) { // FUNCION TOTAL DE CARRITO
     return total
 }
 
-carritoJS = JSON.parse(localStorage.getItem("listaProductos"))///DATOS DE LOCALSTORAGE
+carritoJS = JSON.parse(localStorage.getItem("listaProductos"))//DATOS DE LOCALSTORAGE
 
 
-
+function ShowSelected()
+{
+    let filtr = document.querySelector(".categ")
+    let filt = document.querySelectorAll(".categ")
+    let selected = filtr.options[filtr.selectedIndex].text.toUpperCase()
+    filt.forEach(el => {
+        el.addEventListener("click", (e) => {
+            let categ = (e.target.id)
+            let resultados =  []
+            fetch("./js/data.json")
+                .then((res) => res.json())
+                .then((data) => {
+                    resultados = data.filter(produ => produ.categoria  ===(categ) || produ.nombre.includes(selected))
+                    const ocultar = document.querySelector(".ocultar").style.display = "none";
+                    cargarProductos(resultados,filt)
+                        
+                }); 
+            
+        });
+    })
+    
+    
+}
 
 
