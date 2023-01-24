eeb07c33bb9f68f26607f70a5f3a17e2a0a262a7
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-24 15:09:46 -0300

    151 commit

--- js/tiendaAf.js
+++ js/tiendaAf.js
@@ -176,14 +176,13 @@ function agregarACarrito(id){ //FUNCION PARA AGREGAR PRODUCTOS AL CARRITO
         .then((res) => res.json())
         .then((data) => {
             let productoEncontrado = data.find(prod => prod.id === parseInt(id))
-        if ((productosCarrito.some((el) => el.id == productoEncontrado.id))){
-            productosCarrito.map(el => el.cantidad += 1)
-        }else {
-            productosCarrito.push(productoEncontrado)
-        }
-        guardarLocal("listaProductos",JSON.stringify(productosCarrito.concat(carritoJS)));
-        
+            if ((productosCarrito.some((el) => el.id == productoEncontrado.id))){
+                productosCarrito.map(el => el.cantidad += 1)
+            }else {
+                productosCarrito.push(productoEncontrado)
+            }
         }); 
+        guardarLocal("listaProductos",JSON.stringify(productosCarrito.concat(carritoJS)));
 }
 guardarLocal("listaProductos",JSON.stringify(carritoJS));
 
