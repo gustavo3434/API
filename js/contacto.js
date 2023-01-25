efac481a3e6c02ff7eec6f867bc72c7c23804669
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-25 15:10:35 -0300

    173 commit

--- js/contacto.js
+++ js/contacto.js
@@ -1,28 +1,16 @@
 let enviar = document.querySelector(".enviar")
 const nombre = document.querySelector(".nombre")
 const apellido = document.querySelector(".apellido")
-let motivo = document.querySelector(".motivo")
-let selected = motivo.options[motivo.selectedIndex].text
 
 enviar.addEventListener("click",(e) => {
   e.preventDefault()
   swal({
     title: "Tieanda AF",
-    text: nombre.value.trim()+ " "+apellido.value.trim()+" su "+selected+" se envio con exito, nos pondremos en contacto mediante el mail indicado",
-    buttons: true,
-    dangerMode: true,
+    text: nombre.value.trim().toUpperCase()+ " "+apellido.value.trim().toUpperCase()+" se envio con exito su Reclamo/Opinion, nos pondremos en contacto mediante el mail indicado",
   })
-  .then((will) => {
-    if (will) {
-      swal("La compra fue finalizada con exito, !GRACIAS POR ELEGIR TIENDA AF!", {
-       icon: "success",
-      })
-      setTimeout (redireccionar, 1900);
-      } else {
-      swal("La compra fue cancelada");
-      setTimeout (redireccionar, 1900);
-      }
-  });
+    function redireccionar(){window.location="./index.html";}
+    setTimeout (redireccionar, 1500);
+      
       
 })
 
