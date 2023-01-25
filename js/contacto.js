a88f82acc26316a1ddffd448e5cf3ff24c850abe
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-25 14:16:02 -0300

    169 commit

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,4 @@
-const enviar = document.querySelector(".enviar")
+let enviar = document.querySelector(".enviar")
 const nombre = document.querySelector(".nombre")
 const apellido = document.querySelector(".apellido")
 let motivo = document.querySelector(".motivo")
@@ -12,6 +12,17 @@ enviar.addEventListener("click",(e) => {
     buttons: true,
     dangerMode: true,
   })
+  .then((will) => {
+    if (will) {
+      swal("La compra fue finalizada con exito, !GRACIAS POR ELEGIR TIENDA AF!", {
+       icon: "success",
+      })
+      setTimeout (redireccionar, 1900);
+      } else {
+      swal("La compra fue cancelada");
+      setTimeout (redireccionar, 1900);
+      }
+  });
       
 })
 
