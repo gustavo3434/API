5e254842b052b4cde8c867ab61611c2b22ff5254
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-25 11:38:54 -0300

    160 commit

--- js/carrito.js
+++ js/carrito.js
@@ -71,7 +71,9 @@ if (carritoJS) {
     div1.innerHTML = '<a class="btn btn-dark" href="./formulario_compra.html" role="button">COMPRAR</a>'
     comprar.append(div1)
 
-}else if (carritoJS.length = 0){
+}
+
+if (carritoJS && carritoJS.length === 0) {
     ocul = document.querySelector(".ocultarTot").style.display = "none"
     let carritoVacio = document.createElement(`h2`)
     carritoVacio.innerHTML = 
