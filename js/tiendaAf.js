8ed747796d58a9a270f5eac638947baada5b88d1
gustavoale <gustavo.1@hotmail.com.ar>
2023-01-25 22:07:09 -0300

    185 commit

--- js/tiendaAf.js
+++ js/tiendaAf.js
@@ -150,10 +150,12 @@ const filtrado = (parametro) => { //FUNCION DE FILTRADO DE PRODUCTOS
             .then((res) => res.json())
             .then((data) => {
                 resultados = data.filter(produ => produ.nombre.includes(parametro) || produ.color.includes(parametro))
+                if (resultados.length > 0) {
+                    const ocultar = document.querySelector(".ocultar").style.display = "none";
+                    cargarProductos(resultados,filt)
+                }
             }); 
-            if (resultados.length > 0) {
-                cargarProductos(resultados,filt)
-            }
+            
     } 
 }
 
@@ -199,7 +201,6 @@ function agregarACarrito(id){ //FUNCION PARA AGREGAR PRODUCTOS AL CARRITO
 buscar.addEventListener("click",(e) => {
     e.preventDefault()
     let parametro = inputSearch.value.trim().toUpperCase()
-    const ocultar = document.querySelector(".ocultar").style.display = "none";
     filtrado (parametro)
 })
 
